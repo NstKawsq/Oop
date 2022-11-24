706017a275b564005dc3bed208b861fd2b25e03c
NstKawsq <stfobaeq@gmail.com>
2022-11-25 01:29:00 +0700

    lab6

--- cont.py
+++ cont.py
@@ -1,16 +1,16 @@
 import testing_class_constr
 
 
-class сont():
-    length=100
-    container=['']*length
+class Cont:
+    length = 100
+    container = ['']*length
 
     def input(self, filename='in.txt'):
-        flag=0
-        i=0
+        flag = 0
+        i = 0
         try:
             with open(filename, "r", encoding='utf-8') as fin:
-                with open("out.txt", "w", encoding='utf-8') as fout:
+                with open("out.txt", "w", encoding='utf-8') as fileout:
                     for line in fin.readlines():
                         if i == 100:
                             break
@@ -23,13 +23,16 @@ class сont():
                                             part[3] == '6\n') | (part[3] == '7\n') | (part[3] == '8\n') | (
                                             part[3] == '9\n') | (part[3] == '10\n')):
                                         if part[0] == '0':
-                                            self.container[i] = testing_class_constr.Aforizm(part[0], part[2], part[3], part[1])
+                                            self.container[i] = testing_class_constr.Aforizm(part[0], part[2],
+                                                                                             part[3], part[1])
                                             i += 1
                                         elif part[0] == '1':
-                                            self.container[i] = testing_class_constr.Quot(part[0], part[2], part[3], part[1])
+                                            self.container[i] = testing_class_constr.Quot(part[0], part[2],
+                                                                                          part[3], part[1])
                                             i += 1
                                         elif part[0] == '2':
-                                            self.container[i] = testing_class_constr.Riddle(part[0], part[2], part[3], part[1])
+                                            self.container[i] = testing_class_constr.Riddle(part[0], part[2],
+                                                                                            part[3], part[1])
                                             i += 1
                                     else:
                                         flag = 1
@@ -40,7 +43,7 @@ class сont():
                             else:
                                 flag = 1
                                 print('неверное количество ключевых моментов')
-                        fout.write('Контейнер заполнен')
+                        fileout.write('Контейнер заполнен')
         except FileNotFoundError:
             flag = 1
             print('такого файла не существует!')
@@ -49,106 +52,94 @@ class сont():
             print('файл пустой')
         return i, flag
 
-
     def out(self, filename='out.txt'):
-        type = ''
-        razmernost = 0
+        container_filled_sections = 0
         for i in range(len(self.container)):
             if self.container[i] == "":
-                with open(filename, "a", encoding='utf-8') as fout:
-                    fout.write(f'\nКонтейнер содержит {i} элементов:')
-                    razmernost = i
+                with open(filename, "a", encoding='utf-8') as fileout:
+                    fileout.write(f'\nКонтейнер содержит {i} элементов:')
+                    container_filled_sections = i
                 break
-
-        with open(filename, "a", encoding='utf-8') as fout:
-            if razmernost != 0:
-                for j in range(0, razmernost):
-
-                    # shape in
-                    # type=shape_in.determine_shape(container,j)
-                    self.container[j].printMe(fout)
-
-            elif razmernost == 0:
-                fout.write(f'\nКонтейнер содержит {razmernost} элементов:')
-                fout.write(f'\nКонтейнер очень пуст')
+        with open(filename, "a", encoding='utf-8') as fileout:
+            if container_filled_sections != 0:
+                for j in range(0, container_filled_sections):
+                    self.container[j].printMe(fileout)
+            elif container_filled_sections == 0:
+                fileout.write(f'\nКонтейнер содержит {container_filled_sections} элементов:')
+                fileout.write(f'\nКонтейнер очень пуст')
 
     def clear(self):
-        self.container=[]
+        self.container = []
 
     def mark_count(self, filename='out.txt'):
-        razmernost = 0
+        container_filled_sections = 0
         for i in range(len(self.container)):
             if self.container[i] == "":
-                with open(filename, "a", encoding='utf-8') as fout:
-                    fout.write(f'\nКонтейнер содержит {i} элементов:')
-                    razmernost = i
+                with open(filename, "a", encoding='utf-8') as fileout:
+                    fileout.write(f'\nКонтейнер содержит {i} элементов:')
+                    container_filled_sections = i
                 break
-        mark_example = '",.;:!?)(\/'
+        mark_example = '",.;:!?)(/'
 
-        with open(filename, "a", encoding='utf-8') as fout:
-            for i in range(0, razmernost):
-                punc_count = 0
+        with open(filename, "a", encoding='utf-8') as fileout:
+            for i in range(0, container_filled_sections):
+                punctuation_signs_count = 0
                 for mark in mark_example:
-                    str = self.container[i].content
-                    for j in range(len(str)):
-                        if str[j].find(mark) != -1:
-                            punc_count += 1
+                    temp_str = self.container[i].content
+                    for j in range(len(temp_str)):
+                        if temp_str[j].find(mark) != -1:
+                            punctuation_signs_count += 1
 
-
-                fout.write(f'\nВ строке {i}, содержится {punc_count} знаков препинания')
-                return punc_count
+                fileout.write(f'\nВ строке {i}, содержится {punctuation_signs_count} знаков препинания')
+                return punctuation_signs_count
 
     def mark_for_sort(self, container):
-        punc_count = 0
-        punc_marks = '"",.;:!?\/'
-        for mark in punc_marks:
-            str = container.content
-            for j in range(len(str)):
-                if str[j].find(mark) != -1:
-                    punc_count += 1
-        return punc_count
+        punctuation_signs_count = 0
+        punctuation_mark = '"",.;:!?/'
+        for mark in punctuation_mark:
+            temp_str = container.content
+            for j in range(len(temp_str)):
+                if temp_str[j].find(mark) != -1:
+                    punctuation_signs_count += 1
+        return punctuation_signs_count
 
     def sort(self):
+        container_filled_sections = 0
         for i in range(len(self.container)):
             if self.container[i] == "":
-                razmernost = i
+                container_filled_sections = i
                 break
-
-        for i in range(razmernost - 1):
-            for j in range(razmernost - i - 1):
+        for i in range(container_filled_sections - 1):
+            for j in range(container_filled_sections - i - 1):
                 if self.mark_for_sort(self.container[j]) < self.mark_for_sort(self.container[j+1]):
                     self.container[j], self.container[j+1] = self.container[j+1], self.container[j]
 
     def filtered_output_by_quotation(self, filename='out.txt'):
-        razmernost = 0
+        container_filled_sections = 0
         for i in range(len(self.container)):
             if self.container[i] == "":
-                with open(filename, "a", encoding='utf-8') as fout:
-                    fout.write(f'\nКонтейнер содержит {i} элементов:')
-                    razmernost = i
+                with open(filename, "a", encoding='utf-8') as fileout:
+                    fileout.write(f'\nКонтейнер содержит {i} элементов:')
+                    container_filled_sections = i
                 break
 
-        with open(filename, "a", encoding='utf-8') as fout:
-            if razmernost != 0:
-                for j in range(0, razmernost):
+        with open(filename, "a", encoding='utf-8') as fileout:
+            if container_filled_sections != 0:
+                for j in range(0, container_filled_sections):
                     if self.container[j].index == '0':
-                        self.container[j].printMe(fout)
+                        self.container[j].printMe(fileout)
 
     def filtered_output_by_aforizm(self, filename='out.txt'):
-        razmernost = 0
+        container_filled_sections = 0
         for i in range(len(self.container)):
             if self.container[i] == "":
-                with open(filename, "a", encoding='utf-8') as fout:
-                    fout.write(f'\nКонтейнер содержит {i} элементов:')
-                    razmernost = i
+                with open(filename, "a", encoding='utf-8') as fileout:
+                    fileout.write(f'\nКонтейнер содержит {i} элементов:')
+                    container_filled_sections = i
                 break
 
-        with open(filename, "a", encoding='utf-8') as fout:
-            if razmernost != 0:
-                for j in range(0, razmernost):
-
-                    # shape in
-                    # type=shape_in.determine_shape(container,j)
+        with open(filename, "a", encoding='utf-8') as fileout:
+            if container_filled_sections != 0:
+                for j in range(0, container_filled_sections):
                     if self.container[j].index == '1':
-                        self.container[j].printMe(fout)
-
+                        self.container[j].printMe(fileout)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cont
 
 
-temp=cont.cont()
-flag=temp.input()
-if flag[1]==0:
+temp = cont.Cont()
+flag = temp.input()
+if flag[1] == 0:
     temp.out()
     temp.mark_count()
     temp.sort()
