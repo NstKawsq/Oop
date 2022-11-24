bb1c762ad93f5bbf9ff3b93320c2e91c7d49ca9e
NstKawsq <stfobaeq@gmail.com>
2022-11-25 01:27:21 +0700

    lab5

--- cont.py
+++ cont.py
@@ -1,29 +1,53 @@
 import testing_class_constr
 
-class cont():
+
+class сont():
     length=100
     container=['']*length
 
     def input(self, filename='in.txt'):
+        flag=0
         i=0
-        with open(filename, "r", encoding='utf-8') as fin:
-            with open("out.txt", "w", encoding='utf-8') as fout:
-                for line in fin.readlines():
-                    part = line.split("|")
-                    if part[0] == '0':
-                        self.container[i] = testing_class_constr.Aforizm(part[0], part[2], part[3], part[1])
-                        i += 1
-
-                    elif part[0] == '1':
-                        self.container[i] = testing_class_constr.Quot(part[0], part[2], part[3], part[1])
-                        i += 1
-
-                    elif part[0] == '2':
-
-                        self.container[i] = testing_class_constr.Riddle(part[0], part[2], part[3], part[1])
-                        i += 1
-                fout.write('Контейнер заполнен')
-        return i
+        try:
+            with open(filename, "r", encoding='utf-8') as fin:
+                with open("out.txt", "w", encoding='utf-8') as fout:
+                    for line in fin.readlines():
+                        if i == 100:
+                            break
+                        else:
+                            part = line.split("|")
+                            if len(part) == 4:
+                                if (part[0] == '1') | (part[0] == '0') | (part[0] == '2'):
+                                    if ((part[3] == '0\n') | (part[3] == '1\n') | (part[3] == '2\n') | (
+                                            part[3] == '3\n') | (part[3] == '4\n') | (part[3] == '5\n') | (
+                                            part[3] == '6\n') | (part[3] == '7\n') | (part[3] == '8\n') | (
+                                            part[3] == '9\n') | (part[3] == '10\n')):
+                                        if part[0] == '0':
+                                            self.container[i] = testing_class_constr.Aforizm(part[0], part[2], part[3], part[1])
+                                            i += 1
+                                        elif part[0] == '1':
+                                            self.container[i] = testing_class_constr.Quot(part[0], part[2], part[3], part[1])
+                                            i += 1
+                                        elif part[0] == '2':
+                                            self.container[i] = testing_class_constr.Riddle(part[0], part[2], part[3], part[1])
+                                            i += 1
+                                    else:
+                                        flag = 1
+                                        print('неверная субъективная оценка')
+                                else:
+                                    flag = 1
+                                    print('неверная типизация')
+                            else:
+                                flag = 1
+                                print('неверное количество ключевых моментов')
+                        fout.write('Контейнер заполнен')
+        except FileNotFoundError:
+            flag = 1
+            print('такого файла не существует!')
+        if i == 0:
+            flag = 1
+            print('файл пустой')
+        return i, flag
 
 
     def out(self, filename='out.txt'):
